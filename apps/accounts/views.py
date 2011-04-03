861d81dc4937efa4eccd212db04f45710a5792d9
ezl <ericzliu@gmail.com>
2011-04-03 11:48:40 -0500

    Clean up accounts.views

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -23,17 +23,17 @@ from utils import get_redirect_url
 @csrf_protect
 @never_cache
 def login(request, template_name='accounts/login.html',
-          redirect_field_name='next', authentication_form=AuthenticationForm,
+          authentication_form=AuthenticationForm,
           extra_context={}):
     """
     contrib.auth.views.login, but with a few added benefits: extra_context and
     it redirects to settings.LOGIN_REDIRECT_URL if user is already logged in.
     """
-    redirect_to = get_redirect_url(request, redirect_field_name=redirect_field_name,
-                                   fallback=settings.LOGIN_REDIRECT_URL)
     if request.method == 'POST':
         form = authentication_form(data=request.POST)
         if form.is_valid():
+            redirect_to = get_redirect_url(request, redirect_field_name='next',
+                                           fallback=settings.LOGIN_REDIRECT_URL)
             auth_raw_login(request, form.get_user())
             if request.session.test_cookie_worked():
                 request.session.delete_test_cookie()
@@ -47,23 +47,17 @@ def login(request, template_name='accounts/login.html',
         current_site = RequestSite(request)
     context = {
         'form': form,
-        # 'registration_form': AuthenticationForm(), [ for creating a login/register page]
-        redirect_field_name: redirect_to,
         'site': current_site,
         'site_name': current_site.name
     }
     context.update(extra_context)
     return direct_to_template(request, template_name, context)
 
-
 def logout(request, next_page='/'):
     messages.success(request, "You've been successfully logged out.")
     return auth_logout(request, next_page=next_page)
 
-
-def register(request, redirect_field_name='next', user_type='landlord',
-             template_name='accounts/register.html'):
-    redirect = request.REQUEST.get(redirect_field_name, '')
+def register(request, template_name='accounts/register.html'):
     if request.method == 'POST':
         form = UserCreationForm(data=request.POST)
         if form.is_valid():
@@ -73,10 +67,10 @@ def register(request, redirect_field_name='next', user_type='landlord',
             user = authenticate(username=username, password=password)
             auth_raw_login(request, user)
             messages.success(request, 'Registration successful.')
-            redirect_to = get_redirect_url(request, redirect_field_name=redirect_field_name,
-                                           fallback=settings.LOGIN_REDIRECT_URL)
+            redirect_to = get_redirect_url(request,
+                                           fallback=settings.REGISTER_REDIRECT_URL)
             return HttpResponseRedirect(redirect_to)
-    else:
+    else: # request.method == 'GET'
         form = UserCreationForm()
     return direct_to_template(request, template_name, locals())
 
@@ -85,10 +79,11 @@ def user_settings(request, template_name='accounts/user_settings.html'):
     return direct_to_template(request, template_name, locals())
 
 @login_required
-def change_password(request):
+def change_password(request, template_name='accounts/change_password.html'):
+    redirect_to = reverse('accounts_user_settings')
     resp = auth_password_change(request,
-            post_change_redirect=reverse('accounts_user_settings'),
-            template_name='accounts/change_password.html')
+            post_change_redirect=redirect_to,
+            template_name=template_name)
     if resp.status_code == 302:
         # auth_password_change is redirecting, so it succeeded
         messages.success(request, 'Your password has been changed successfully.')
